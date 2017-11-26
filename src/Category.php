bf9d8b97ad49272a50fff91461d64c6fa506affc
Balancer <balancer@balancer.ru>
2017-11-26 22:07:21 +0300

    Category base class.

---
+++ src/Category.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace B2\Vanilla;
+
+class Category extends ObjectDb
+{
+	function table_name() { return 'GDN_Category'; }
+	function table_fields()
+	{
+		return [
+			'id' => 'CategoryID',
+			'ParentCategoryID',
+			'TreeLeft',
+			'TreeRight',
+			'Depth',
+			'CountDiscussions',
+			'CountComments',
+			'DateMarkedRead',
+			'AllowDiscussions',
+			'Archived',
+			'title' => 'Name',
+			'UrlCode',
+			'Description',
+			'Sort',
+			'CssClass',
+			'Photo',
+			'PermissionCategoryID',
+			'PointsCategoryID',
+			'HideAllDiscussions',
+			'DisplayAs',
+			'InsertUserID',
+			'UpdateUserID',
+			'DateInserted',
+			'DateUpdated',
+			'LastCommentID',
+			'LastDiscussionID',
+			'LastDateInserted',
+			'AllowedDiscussionTypes',
+			'DefaultDiscussionType',
+			'AllowFileUploads',
+		];
+	}
+}
